f714b5b9d7a5a81c311b8d496930db96dfc6af2f
InfiniteTrident23 <siddharthnigam002@gmail.com>
2025-04-11 22:51:22 +0530

    first commit

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello from Home Page</h1>
+      <h1>Hello from Dashboard</h1>
     </div>
   );
 }
---
+++ components/general/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+export function Navbar(){
+    return (
+        <nav className="py-5 flex item-center justify-between">
+            <div className="flex items-center gap-6">
+                <Link href="/Blog" className="text-blue-500 hover:text-gray-900">
+                    <h1 className="text-2xl font-bold">Blog</h1>
+                </Link>
+            <Link href="/">
+                    <h1>
+                        <span className="text-2xl font-bold text-blue-500">Siddharth</span>
+                    </h1>
+                </Link>
+                <Link href="/Dashboard" className="text-2xl font-bold text-blue-500 hover:text-gray-900">
+                    Dashboard
+                </Link>
+            </div>
+            <div className="flex items-center gap-4">
+                <button>Sign Up</button>
+            </div>
+        </nav>
+    )
+}
