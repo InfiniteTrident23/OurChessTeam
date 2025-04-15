64912c00fb0b58eaf80ef2f5a7e0a028ef73e3f9
InfiniteTrident23 <siddharthnigam002@gmail.com>
2025-04-15 23:29:57 +0530

    Adding User model

---
+++ prisma/migrations/20250415175811_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "BlogPost" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    "authorId" TEXT,
+    "authorName" TEXT,
+    "authorImage" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "BlogPost_pkey" PRIMARY KEY ("id")
+);
